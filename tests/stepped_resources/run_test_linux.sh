92c8988fc9c885a2d04f9b131d4bdf7e0903dbcf
Clifford Cheefoon <cheefoonclifford@yahoo.com>
2026-03-14 19:09:21 -0400

    proper flush cache

--- tests/stepped_resources/run_test_linux.sh
+++ tests/stepped_resources/run_test_linux.sh
@@ -6,9 +6,10 @@ for run in {1..5}; do
     # Recursively scan for all files, excluding results.log and run_test.sh
     for file in $(find . -type f -not -name "results.log" -not -name "run_test.sh"); do
         echo "Running docker compose for $file in run $run" 
+        # flush cache to ensure more consistent results
+        echo 3 > /proc/sys/vm/drop_caches
         # Run docker compose and record output
-        sudo echo 3 > /proc/sys/vm/drop_caches
-        sudo docker compose -f "$file" up --abort-on-container-exit >> results.log 2>&1
+        docker compose -f "$file" up --abort-on-container-exit >> results.log 2>&1
         echo "Finished $file in run $run"
     done
     echo "Completed run $run"
