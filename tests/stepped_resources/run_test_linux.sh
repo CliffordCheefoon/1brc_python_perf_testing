aca4f3960207d15ead0bb2b35c3540d2b45542c7
Clifford Cheefoon <cheefoonclifford@yahoo.com>
2026-03-14 20:58:51 -0400

    add timeout to testing

--- tests/stepped_resources/run_test_linux.sh
+++ tests/stepped_resources/run_test_linux.sh
@@ -9,7 +9,7 @@ for run in {1..5}; do
         # flush cache to ensure more consistent results
         echo 3 > /proc/sys/vm/drop_caches
         # Run docker compose and record output
-        docker compose -f "$file" up --abort-on-container-exit >> results.log 2>&1
+        timeout 10m docker compose -f "$file" up --abort-on-container-exit >> results.log 2>&1
         echo "Finished $file in run $run"
     done
     echo "Completed run $run"
